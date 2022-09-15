e250cab8efb80acb78f6130e9c323daaa331165b
Saul Rodriguez <srodriguez@tms.ninja>
2022-09-15 08:03:40 -0500

    Initial Commit

---
+++ Startup.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WelcomeUser.Common;
+using WelcomeUser.Mail;
+
+namespace Microsoft.BotBuilderSamples
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public Startup(IConfiguration config)
+        {
+           Configuration = config;
+        }
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+          
+            services.AddControllers().AddNewtonsoftJson();
+
+            services.AddTransient<IMailService, MailService>();
+            
+            //services.AddTransient<IAppServices, AppServices>();
+
+            services.Configure<MailSettings>(Configuration.GetSection("MailSettings"));
+
+            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
+            //services.Configure<ApiKey>(Configuration.GetSection("AppSettings"));
+            services.AddSingleton<AppServices>();
+            services.AddSingleton<AppSettings>();
+            services.AddSingleton(Configuration);
+
+            // Create the Bot Framework Adapter with error handling enabled.
+            services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
+
+            // Create the storage we'll be using for User and Conversation state. (Memory is great for testing purposes.)
+            services.AddSingleton<IStorage, MemoryStorage>();
+
+            // Create the User state.
+            services.AddSingleton<UserState>();
+
+            // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+            services.AddTransient<IBot, WelcomeUserBot>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles()
+                .UseStaticFiles()
+                .UseRouting()
+                .UseAuthorization()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
+
+            // app.UseHttpsRedirection();
+        }
+    }
+}
